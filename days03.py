662040fb60871cecc9eea9690ce6058e103433f6
susantraut1 <techhube027@gmail.com>
2025-01-10 12:57:50 +0530

    it's make change 52 to last ones still changes

--- days03.py
+++ days03.py
@@ -36,13 +36,87 @@
 #     print("user is not adult")
 
 
-score = int(input("ENTER YOUR score :  "))
+# score = int(input("ENTER YOUR score :  "))
 
-if score >= 100:
-    print("Grade : A")
-elif score >= 80:
-    print("Grade : B")
-elif score >= 50:
-    print("Grade : C")
-else:
-    print("Grade : D")
+# if score >= 100:
+#     print("Grade : A")
+# elif score >= 80:
+#     print("Grade : B")
+# elif score >= 50:
+#     print("Grade : C")
+# else:
+#     print("Grade : D")
+
+# x = 15 
+# y = 5
+
+# if x > y :
+#     print("x is also greater than y")
+#     if x >15:
+#       print("x is not greater than 15")
+#     else:
+#         print("x is not geater than 15")
+# else:
+#     print("x is not greater than y")
+
+
+# now we write for loops
+
+# fruits = ['apple','banana','chery','dargon fruit']
+
+# for  x  in fruits:
+#     print(x)
+
+# number= [1,2,3,4,5,6]
+
+# for  i in number:
+#     print(i)
+
+
+# items = [int,float,'munna',5,6,6.6]
+
+# for x in items:
+#   if  isinstance(x,(int,str,float)):
+#      print(x)
+
+# items = [int,float,'munna',5,6,6.6]
+
+# for x in items:
+#   if  isinstance(x,(int,float)) and x >=6:
+#       print(x)
+
+# item = {'name':'susan','age':30 , 'city':'karnataka'}
+
+# for key,value in item.items():
+#      print(f'{key}:{value}')    
+    
+
+for i in range (5):
+    if i == 3:
+        break
+    print(i)
+
+# while loop conditiion
+
+# count = 0 
+# while count < 10:
+#     print(count)
+#     count += 1
+    
+
+# i = 0
+# while (i < 10 ):
+#     print(i+1 , end = "")
+#     i += 1
+    
+    
+i = 0 
+# while (True):
+#     print(i + 1, end = "")
+#     i = +1
+#     if i >= 10:
+#          break
+    
+
+# foor loop
+
