b7361cd716f211a21d1f30f937d750053f6ac80c
susantraut1 <techhube027@gmail.com>
2025-01-15 00:08:44 +0530

    system shutdown code will add

--- day05.py
+++ day05.py
@@ -28,15 +28,45 @@
         
 # this is the webscraping
 
-import requests
-from bs4 import BeautifulSoup # type: ignore
+# import requests
+# from bs4 import BeautifulSoup # type: ignore
 
-url = 'https://www.netflix.com/in/'
-response = requests.get(url)
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
-    links = [link['href'] for link in soup.find_all('a')]
-    print(links)
+# url = 'https://www.netflix.com/in/'
+# response = requests.get(url)
+# if response.status_code == 200:
+#     soup = BeautifulSoup(response.text, 'html.parser')
+#     links = [link['href'] for link in soup.find_all('a')]
+#     print(links)
 
-else:
-    print(f"faild to fetch the url. status code :{response.staus_code}")
+# else:
+#     print(f"faild to fetch the url. status code :{response.staus_code}")
+
+    
+    
+    
+import schedule
+import time
+
+def job():
+    print("This is a scheduled task.")
+
+# Schedule the job to run at 5:51 AM daily
+schedule.every().day.at("05:51").do(job)
+
+# Keep the script running to check for scheduled tasks
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
+
+#system shutdown 
+
+
+# import os
+
+# os.system("shutdown /s /t 0")
+# # don't execute the code otherwise system will shutdown
+
+
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,13 @@
-count += 1
+import schedule
+# import time
+
+# def job():
+#     print("This is a scheduled task.")
+
+# # Schedule the job to run at 5:51 AM daily
+# schedule.every().day.at("05:51").do(job)
+
+# # Keep the script running to check for scheduled tasks
+# while True:
+#     schedule.run_pending()
+#     time.sleep(1)
